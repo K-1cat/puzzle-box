14d7d93fc7184026a657e036daeec193c324fa80
Kanoa Krohn <ksk35@students.uwf.edu>
2026-04-11 09:58:58 -0500

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configs
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'puzzle-box-secret-key-2026')
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "..", "instance", "puzzle_box.db")}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['UPLOAD_FOLDER'] = os.path.join(basedir, '..', 'uploads')
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from app.models import User
+
+    # Import models to register them with SQLAlchemy
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    # Blueprints
+    from app.routes.main import main_bp
+    app.register_blueprint(main_bp)
+
+    # Create database tables
+    with app.app_context():
+        db.create_all()
+
+    return app
---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/puzzle_box.db'
+    UPLOAD_FOLDER = 'uploads'
